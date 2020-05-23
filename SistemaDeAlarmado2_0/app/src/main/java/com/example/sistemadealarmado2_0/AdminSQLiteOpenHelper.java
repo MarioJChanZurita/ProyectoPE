d2d2573d5aebca4ff16ef14cb1f05a6ef7b91365
ddgant <60008674+ddgant@users.noreply.github.com>
2020-05-23 11:52:28 -0500

    Update AdminSQLiteOpenHelper.java

--- SistemaDeAlarmado2_0/app/src/main/java/com/example/sistemadealarmado2_0/AdminSQLiteOpenHelper.java
+++ SistemaDeAlarmado2_0/app/src/main/java/com/example/sistemadealarmado2_0/AdminSQLiteOpenHelper.java
@@ -13,7 +13,7 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(" create table alarma( idal integer primary key autoincrement,encabezado text, mensaje text,fecha date, hora time)");
+        db.execSQL(" create table alarma( idal integer primary key autoincrement,encabezado text, mensaje text,dosis text,duracion number,fecha date, hora time)");
 
 
 
