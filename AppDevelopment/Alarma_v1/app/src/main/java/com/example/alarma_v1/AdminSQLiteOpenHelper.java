eab17e25cfc072111177a950c32427d7350afbc2
Mario J. Chan Zurita <56327994+MarioJChanZurita@users.noreply.github.com>
2020-06-03 17:41:35 -0500

    Alarma_v1

---
+++ AppDevelopment/Alarma_v1/app/src/main/java/com/example/alarma_v1/AdminSQLiteOpenHelper.java
@@ -0,0 +1,45 @@
+package com.example.alarma_v1;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase BaseDatos) {
+        BaseDatos.execSQL("create table datos(" +
+                "posicion int primary key, nombre text, periodoHoras int, periodoMinutos int, duracion int,notas text, fecha date, hora time)");
+        /**
+         * posicion = numero de medicina agregada
+         * nombre = nombre de la medicina
+         * periodoHoras = horas que deberian pasar para tomar la medicina
+         * periodoMinutos = minutos sobre la hora que deberian pasar para tomar la medicina
+         * duracion = dias que el usuario tomara la medicina
+         * notas = notas adicionaes
+         * fecha = fecha a partir de la cual la alarma se activará
+         * hora = hora a partir de la cual la alarma se activará
+         */
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists datos");
+        db.execSQL("create table datos(" +
+                "posicion int primary key, nombre text, periodoHoras int, periodoMinutos int, duracion int,notas text, fecha date, hora time)");
+        /**
+         * posicion = numero de medicina agregada
+         * nombre = nombre de la medicina
+         * periodoHoras = horas que deberian pasar para tomar la medicina
+         * periodoMinutos = minutos sobre la hora que deberian pasar para tomar la medicina
+         * duracion = dias que el usuario tomara la medicina
+         * notas = notas adicionaes
+         * fecha = fecha a partir de la cual la alarma se activará
+         * hora = hora a partir de la cual la alarma se activará
+         */
+    }
+}
---
+++ AppDevelopment/Alarma_v1/app/src/main/java/com/example/alarma_v1/Alarma/AlertReceiver.java
@@ -0,0 +1,55 @@
+package com.example.alarma_v1.Alarma;
+
+import android.app.DatePickerDialog;
+import android.content.BroadcastReceiver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import androidx.core.app.NotificationCompat;
+
+import com.example.alarma_v1.AdminSQLiteOpenHelper;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class AlertReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Calendar calendarioHora = Calendar.getInstance();
+        Calendar calendarioFecha = Calendar.getInstance();
+
+        //Obtenemos la fecha del sistema
+        calendarioFecha.get(Calendar.DAY_OF_YEAR);
+        String fecha_sistema = DateFormat.getDateInstance(DateFormat.FULL).format(calendarioFecha.getTime());
+
+        //Obtenemos la hora del sistema
+        calendarioHora.get(Calendar.HOUR_OF_DAY);
+        calendarioHora.get(Calendar.MINUTE);
+        String hora_sistema = DateFormat.getTimeInstance(DateFormat.SHORT).format(calendarioHora.getTime());
+
+        //Llamamos a la base de datos para sacar los valores de fecha y hora introducidos por el usuario
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(context, "administracion",null,1);
+        SQLiteDatabase baseDatos = admin.getWritableDatabase();
+
+        //Hacemos una comparación entre las fechas y horas para activar la notificacion
+        if(baseDatos!=null){
+            Cursor fila = baseDatos.rawQuery("SELECT * FROM datos WHERE fecha ='"+fecha_sistema+"' AND hora = '"+hora_sistema+"'",null);
+            if(fila.moveToFirst()){
+
+                //Obtenemos los valores de nombre de alarma y notas para mostrar en la notificacion
+                String nombre = fila.getString(1);
+                String mensaje = fila.getString(5);
+
+                //Llamamos a la clase de la notificacion para crearla y mandarla
+                NotificationHelper notificationHelper = new NotificationHelper(context);
+                NotificationCompat.Builder nb = notificationHelper.getNotification(nombre, mensaje);
+                notificationHelper.getManager().notify(1,nb.build());
+            }
+
+        }
+        baseDatos.close();
+    }
+}
