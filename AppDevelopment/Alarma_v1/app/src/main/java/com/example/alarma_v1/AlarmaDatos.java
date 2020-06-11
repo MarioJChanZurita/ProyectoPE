7d0001380caab887534aef70bb0b0e9dd0a82d07
Mario J. Chan Zurita <56327994+MarioJChanZurita@users.noreply.github.com>
2020-06-10 23:37:03 -0500

    Ya funciona el editar

--- AppDevelopment/Alarma_v1/app/src/main/java/com/example/alarma_v1/AlarmaDatos.java
+++ AppDevelopment/Alarma_v1/app/src/main/java/com/example/alarma_v1/AlarmaDatos.java
@@ -1,24 +1,43 @@
 package com.example.alarma_v1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.AlarmManager;
+import android.app.DatePickerDialog;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
-import java.util.ArrayList;
+import com.example.alarma_v1.Alarma.AlertReceiver;
+import com.example.alarma_v1.Alarma.DatePickerFragment;
+import com.example.alarma_v1.Alarma.TimePickerFragment;
 
-public class AlarmaDatos extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class AlarmaDatos extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener{
 
     private EditText nombreMedicina, horasMedicina, minutosMedicina, duracionMedicina, notasMedicina;
     private Spinner nombreAlarmas;
+    TextView mostrarFecha, mostrarHora;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +51,29 @@ public class AlarmaDatos extends AppCompatActivity {
         minutosMedicina = (EditText)findViewById(R.id.minutosMedicina);
         duracionMedicina = (EditText)findViewById(R.id.duracionMedicina);
         notasMedicina = (EditText)findViewById(R.id.notasMedicina);
+
+        mostrarFecha = (EditText) findViewById(R.id.fechaSeleccionada);
+        mostrarHora = (EditText) findViewById(R.id.horaSeleccionada);
+
+        //boton para abrir el date picker y seleccionar la fecha
+        Button btDate = (Button) findViewById(R.id.seleccionarFecha);
+        btDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date picker");
+            }
+        });
+
+        //boton para abrir el time picker y seleccionar la hora
+        Button btTime = (Button) findViewById(R.id.seleccionarHora);
+        btTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(), "time picker");
+            }
+        });
 
         spinnerAlarmas(generarLista());
     }
@@ -92,6 +134,9 @@ public class AlarmaDatos extends AppCompatActivity {
                 minutosMedicina.setText(datosAlarma.getString(3));
                 duracionMedicina.setText(datosAlarma.getString(4));
                 notasMedicina.setText(datosAlarma.getString(5));
+
+                mostrarFecha.setText(datosAlarma.getString(6));
+                mostrarHora.setText(datosAlarma.getString(7));
             }
         }
     }
@@ -108,6 +153,9 @@ public class AlarmaDatos extends AppCompatActivity {
         String dura = duracionMedicina.getText().toString();
         String nota = notasMedicina.getText().toString();
 
+        String fecha = mostrarFecha.getText().toString();
+        String hora = mostrarHora.getText().toString();
+
         //verificamos que los campos estan llenos
         if(!nombre.isEmpty() && !hrs.isEmpty() && !min.isEmpty() && !dura.isEmpty()){
             //Convertimos los datos para coincidir con los de la base de datos
@@ -122,7 +170,13 @@ public class AlarmaDatos extends AppCompatActivity {
             modificar.put("duracion", duracion);
             modificar.put("notas", nota);
 
+            modificar.put("fecha", fecha);
+            modificar.put("hora", hora);
+
             int modificacion = baseDatos.update("datos", modificar,"posicion="+opcion, null);
+
+            startAlarm();
+
             //Vemos si se modifico la base de datos
             if(modificacion == 1){
                 Toast.makeText(this,"Cambios guardados", Toast.LENGTH_SHORT).show();
@@ -135,4 +189,43 @@ public class AlarmaDatos extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.YEAR, year);
+
+        updateDateText(c);
+    }
+
+    private void updateDateText(Calendar c) {
+        String dateText = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+        mostrarFecha.setText(dateText);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Calendar c = Calendar.getInstance();
+
+        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        c.set(Calendar.MINUTE, minute);
+        c.set(Calendar.SECOND, 0);
+
+        updateTimeText(c);
+    }
+
+    private void updateTimeText(Calendar c) {
+        String timeText = DateFormat.getTimeInstance(DateFormat.SHORT).format(c.getTime());
+        mostrarHora.setText(timeText);
+    }
+
+    private void startAlarm() {
+        AlarmManager alarmManager = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(),10*1000, pendingIntent);
+
+    }
 }
