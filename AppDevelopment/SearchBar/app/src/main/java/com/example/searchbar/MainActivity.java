c1d337503fadaacbb6aef9231709a39fc2405897
Mario J. Chan Zurita <56327994+MarioJChanZurita@users.noreply.github.com>
2020-06-14 22:03:56 -0500

    searchbar

---
+++ AppDevelopment/SearchBar/app/src/main/java/com/example/searchbar/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.searchbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayAdapter<String> arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.my_list);
+        List<String> mylist = new ArrayList<>();
+        mylist.add("Eraser"); mylist.add("Books");
+        mylist.add("Pencil"); mylist.add("Pen");
+        mylist.add("Copy"); mylist.add("Ruler");
+
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mylist);
+        listView.setAdapter(arrayAdapter);
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.search_icon);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Searh Here");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                arrayAdapter.getFilter().filter(newText);
+                return true;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+
+    }
+}
